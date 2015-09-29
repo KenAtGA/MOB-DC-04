83cb8277537ec5f4c6e4c44eaeb0175e370944c8
KenAtGA <Jackson@Kendricks-MacBook-Air.local>
2015-09-28 21:04:32 -0400

    Created first project under lesson 1

---
+++ Classwork/Lesson 1/Sup world/Sup world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sup world
+//
+//  Created by Kendrick on 9/28/15.
+//  Copyright © 2015 Kendrick Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
