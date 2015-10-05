338bae81521ff3a0e10a3db2a42168924f646690
KenAtGA <Jackson@Kendricks-MacBook-Air.local>
2015-10-05 16:41:46 -0400

    Week 1 Classwork

---
+++ Classwork/Lesson 2/Project 2.1/Navigation Controller/Navigation Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Controller
+//
+//  Created by Kendrick on 9/30/15.
+//  Copyright © 2015 Kendrick Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Homework/Login Project/Login Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login Project
+//
+//  Created by Kendrick on 10/5/15.
+//  Copyright © 2015 Kendrick Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
